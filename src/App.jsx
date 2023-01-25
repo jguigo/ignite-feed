9c3c5d002f37715d79af7ec7305efbacf19ca715
jguigo <jguilhermefmelo21@gmail.com>
2023-01-24 22:17:06 -0300

    🎨 style: Global css config

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Header from './components/Header';
 import { Post } from './Post';
+
+import './global.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Guilherme Melo"
         content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus, optio ad? Quaerat error eius porro dolores eos consectetur repellat saepe dolorem ipsam, nostrum, iusto perferendis ratione facilis repellendus quisquam quae."
