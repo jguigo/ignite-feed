01fee1f5bc39c9e23e855c712fe242700f7f28d9
jguigo <jguilhermefmelo21@gmail.com>
2023-01-24 21:35:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from './Post';
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Guilherme Melo"
+        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus, optio ad? Quaerat error eius porro dolores eos consectetur repellat saepe dolorem ipsam, nostrum, iusto perferendis ratione facilis repellendus quisquam quae."
+      />
+      <Post
+        author="Larissa Mendes"
+        content="pipipipppopopopo BURGILpipipipppopopopo BURGILpipipipppopopopo BURGILpipipipppopopopo BURGILpipipipppopopopo BURGIL"
+      />
+    </div>
+  );
+}
