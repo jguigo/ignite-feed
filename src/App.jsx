dfa8f119ba7e62a6225f92813a489c4ee5032037
jguigo <jguilhermefmelo21@gmail.com>
2023-01-25 22:43:38 -0300

    📦 feat: Adding profile sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import Header from './components/Header';
 import { Post } from './Post';
 
-import './global.css'
+import './global.css';
+
+import styles from './App.module.css';
+import Sidebar from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Guilherme Melo"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus, optio ad? Quaerat error eius porro dolores eos consectetur repellat saepe dolorem ipsam, nostrum, iusto perferendis ratione facilis repellendus quisquam quae."
-      />
-      <Post
-        author="Larissa Mendes"
-        content="pipipipppopopopo BURGILpipipipppopopopo BURGILpipipipppopopopo BURGILpipipipppopopopo BURGILpipipipppopopopo BURGIL"
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Guilherme Melo"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus, optio ad? Quaerat error eius porro dolores eos consectetur repellat saepe dolorem ipsam, nostrum, iusto perferendis ratione facilis repellendus quisquam quae."
+          />
+          <Post
+            author="Larissa Mendes"
+            content="pipipipppopopopo BURGILpipipipppopopopo BURGILpipipipppopopopo BURGILpipipipppopopopo BURGILpipipipppopopopo BURGIL"
+          />
+        </main>
+      </div>
     </div>
   );
 }
