35b842c5a064a22ea2f4814032193a44c71330ef
jguigo <jguilhermefmelo21@gmail.com>
2023-01-28 23:09:32 -0300

    📦 feat: Adding post component without comments section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Header from './components/Header';
-import { Post } from './Post';
 
 import './global.css';
 
 import styles from './App.module.css';
 import Sidebar from './components/Sidebar';
+import Post from './components/Post';
 
 export function App() {
   return (
@@ -14,14 +14,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Guilherme Melo"
-            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus, optio ad? Quaerat error eius porro dolores eos consectetur repellat saepe dolorem ipsam, nostrum, iusto perferendis ratione facilis repellendus quisquam quae."
-          />
-          <Post
-            author="Larissa Mendes"
-            content="pipipipppopopopo BURGILpipipipppopopopo BURGILpipipipppopopopo BURGILpipipipppopopopo BURGILpipipipppopopopo BURGIL"
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
---
+++ src/components/Post.jsx
@@ -0,0 +1,30 @@
+import styles from './Post.module.css';
+
+const Post = () => {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/jguigo.png" />
+          <div className={styles.authorInfo}>
+            <strong>Guilherme Melo</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title='11 de maio às 08:13' dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala pessoal 👋</p>
+        <p>Finalmente finalizei meu novo site/portfólio. Foi um baita desafio criar todo o design e codar na unha, mas consegui 💪🏻</p>
+        <p>Acesse e deixe seu feedback 👉{' '}<a href='#'>devonlane.design</a></p>
+        <p>
+          <a href='#'>#uiux</a>{' '}
+          <a href='#'>#userexperience</a>
+        </p>
+      </div>
+    </article>
+  );
+};
+
+export default Post;
